a6635a1871a9722b5ced366f4535cff2fd90a8ca
jwkeena <jwkeena@gmail.com>
2021-07-21 22:52:05 -0400

    Remove unwanted properties from cards; consolidate cards to one file

--- js/helpers/sort-cards-script.js
+++ js/helpers/sort-cards-script.js
@@ -1,5 +1,5 @@
 // First, get card list for set from https://pokemontcg.io/cards?setCode=base1&pageSize=1000
-// Then convert from JSON here https://json-to-js.com/
+// Then convert from JSON here https://www.convertonline.io/convert/json-to-js
 // Then copy the array as a variable cards here
 const cards =[] 
 
@@ -14,7 +14,10 @@ console.log(sorted);
 
 // Copy this whole file into the dev tools console
 // Mark all the holo rares
+// delete unwanted properties with next line
+set.forEach(card => {delete card.nationalPokedexNumber; delete card.evolvesFrom; delete card.retreatCost; delete card.convertedRetreatCost; delete card.ability; delete card.attacks; delete card.weaknesses; delete card.hp; delete card.resistances; delete card.text; })
+// Add result to cards.js file
+
 // Get the set info from unused-sets.js
 // Copy to sets.js, add new properties. 
 // Add info for packOrder and probability from https://pokemonboosterpack.com/pages/about.html
-// Add new set js file to index.html
